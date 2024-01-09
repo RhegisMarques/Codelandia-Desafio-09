d04e632b694abf11024511acc2bd338fe84405f3
Rhegis Marques <rhegismarques@gmail.com>
2024-01-09 19:38:02 -0300

    up

--- js/jscript.js
+++ js/jscript.js
@@ -11,3 +11,10 @@ menu.onclick =() => {
     menu.classList.toggle('bx-x');
     navbar.classList.toggle('active');
 };
+
+
+window.onscroll =() => {
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
+};
+
